395812ae90db8ea451a84f1bd43865cbd1aaa5ea
Matthew Slanaker <matt.slanaker@gmail.com>
2022-02-11 15:09:48 -0500

    update day-4

--- Smoothstack Homework/day-4-a.py
+++ Smoothstack Homework/day-4-a.py
@@ -20,18 +20,50 @@ hello("Google")
 
 # 4.define a function func4(x,y) which returns the product of both the values.
 
+def func4(x, y):
+    return x + y
+
+
+print(func4(5, 10))
 
 # 5.define a function called as is_even that takes one argument , which returns true when even values is passed and false if it is not.
 
 
 # 6.define a function that takes two arguments ,and returns true if the first value is greater than the second value and returns false if it is less than or equal to the second.
 
+def check_greater(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+
+print(check_greater(65, 75))
 
 # 7.Define a function which takes arbitrary number of arguments and returns the sum of the arguments.
 
 
+def sum_of(*args):
+    addition = 0
+    for arg in args:
+        addition += arg
+    return addition
+
+
+print(sum_of(5, 10, 15, 20))
+
+
 # 8.Define a function which takes arbitrary number of arguments and returns a list containing only the arguments that are even.
 
+def even(*args):
+    even_list = []
+    for i in list(args):
+        if not i % 2:
+            even_list.append(i)
+    return even_list
+
+
+print(even(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 
 # 9.Define a function that takes a string and returns a matching string where every even letter is uppercase and every odd letter is lowercase
 # <<give the python notebook exercises which are discussed  in .ipyb>>
